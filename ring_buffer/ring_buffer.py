9146b38febd11224db6afbcee4214d9e97b655d2
JWNicholson <john.nicholson812@gmail.com>
2020-07-17 11:55:05 -0400

    ring buffer finished

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,23 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.ring_buffer = []
+        self.current_node = 0
+
+    # check if ring_buffer (chosen name of the storage list) is full -
+    # if it's not full add the item to the tail
+    # if it is full remove the head (it is the oldest item)
+    # add the item to the tail after the space is available
+    # if removed head is current set current to tail
 
     def append(self, item):
-        pass
+        if len(self.ring_buffer) == self.capacity:
+            self.ring_buffer.pop(self.current_node)
+            self.ring_buffer.insert(self.current_node, item)
+            self.current_node = (self.current_node + 1) % self.capacity
+
+        else:
+            self.ring_buffer.append(item)
 
     def get(self):
-        pass
+        return self.ring_buffer
